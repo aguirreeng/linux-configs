17c1be1155fdb746fda290722e388f29a8651ce2
Joao Aguirre <aguirre.eng@gmail.com>
2019-01-11 13:22:31 -0200

    Update CEntOS configs

--- machine-install-centos.sh
+++ machine-install-centos.sh
@@ -1,4 +1,4 @@
-# Install and configure Miniconda
+# Install and configure Miniconda.
 wget --directory-prefix=$HOME/Downloads https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
 echo -ne '\n' | bash $HOME/Downloads/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/apps/miniconda
 cp $HOME/.bashrc $HOME/.bashrc_install_backup
@@ -8,15 +8,14 @@ $HOME/apps/miniconda/bin/conda config --add channels conda-forge
 $HOME/apps/miniconda/bin/conda update --prefix $HOME/apps/miniconda conda --yes
 $HOME/apps/miniconda/bin/conda update --prefix $HOME/apps/miniconda --all --yes
 
-# Create conda environment
+# Create conda environment.
 $HOME/apps/miniconda/bin/conda env create --file=$HOME/repository/linux-configs/conda-envs/notebook-environment.yml
 mkdir -p $HOME/apps/miniconda/envs/notebook/etc/conda/activate.d
 mkdir -p $HOME/apps/miniconda/envs/notebook/etc/conda/deactivate.d
 cp $HOME/repository/linux-configs/conda-envs/activate-env_vars.sh $HOME/apps/miniconda/envs/notebook/etc/conda/activate.d/env_vars.sh
 cp $HOME/repository/linux-configs/conda-envs/deactivate-env_vars.sh $HOME/apps/miniconda/envs/notebook/etc/conda/deactivate.d/env_vars.sh
 
-# Thonny
+# Thonny.
 echo -ne '\ny\n' | bash <(curl -s http://thonny.org/installer-for-linux)
 echo "# Thonny path" >> $HOME/.bashrc
 echo "export PATH=\$HOME/apps/thonny/bin:\$PATH" >> $HOME/.bashrc
-
--- machine-install-sudo-centos.sh
+++ machine-install-sudo-centos.sh
@@ -1,23 +1,23 @@
 # Include EPEL repository for additional software
-yum --assumeyes install epel-release
+yum install epel-release --assumeyes
 
 # Develpment libraries and tools
-yum --assumeyes group install "Development Tools"
-yum install kernel-devel.x86_64
-yum install gmp.x86_64 gmp-devel.x86_64
-yum install mpfr.x86_64 mpfr-devel.x86_64
-yum install libmpc.x86_64 libmpc-devel.x86_64
-yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-CPAN perl-devel
-yum install libpng.x86_64 libpng-devel.x86_64
-yum install mesa-libGL.x86_64 mesa-libGL-devel.x86_64
-yum install mesa-libGLU.x86_64 mesa-libGLU-devel.x86_64
-yum install giflib.x86_64 giflib-devel.x86_64
-yum install freeglut.x86_64 freeglut-devel.x86_64
-yum install libharu.x86_64 libharu-devel.x86_64
+#yum  group install "Development Tools" --assumeyes
+#yum  install kernel-devel.x86_64 --assumeyes
+#yum install gmp.x86_64 gmp-devel.x86_64 --assumeyes
+#yum install mpfr.x86_64 mpfr-devel.x86_64 --assumeyes
+#yum install libmpc.x86_64 libmpc-devel.x86_64 --assumeyes
+#yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-CPAN perl-devel --assumeyes
+#yum install libpng.x86_64 libpng-devel.x86_64 --assumeyes
+#yum install mesa-libGL.x86_64 mesa-libGL-devel.x86_64 --assumeyes
+#yum install mesa-libGLU.x86_64 mesa-libGLU-devel.x86_64 --assumeyes
+#yum install giflib.x86_64 giflib-devel.x86_64 --assumeyes
+#yum install freeglut.x86_64 freeglut-devel.x86_64 --assumeyes
+#yum install libharu.x86_64 libharu-devel.x86_64 --assumeyes
 
 # Visual Studio Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-yum check-update
-yum --assumeyes install code
+yum check-update --assumeyes
+yum install code --assumeyes
 
